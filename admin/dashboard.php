d07b130767e622e5f20f4b4c069b3cbf1f15a589
unknown <nandasyarla58911@gmail.com>
2023-05-26 16:14:24 +0700

    Halaman Admin

---
+++ admin/dashboard.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="styleadmin.css">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <title>BINATO (FP)</title>
+</head>
+
+<body>
+    <div class="sidebar close">
+
+        <div class="logo-details">
+            <i class='bx bxs-washer'></i>
+            <span class="logo_name">Database</span>
+
+        </div>
+
+        <ul class="nav-links">
+            <li>
+                <a href="<?php echo "dashboard.php"; ?>" class="active">
+                    <i class='bx bxs-home-smile'></i>
+                    <span class="link_name">Home</span>
+                </a>
+                <ul class="sub-menu blank">
+                    <li><a class="link_name" href="<?php echo "admin/dashboard.php"; ?>">Home</a></li>
+                </ul>
+            </li>
+            <li>
+                <div class="iocn-link">
+                    <a href=" # ">
+                        <i class='bx bxs-user'></i>
+                        <span class="link_name">Pelanggan</span>
+                    </a>
+                    <i class='bx bxs-chevron-down arrow'></i>
+                </div>
+                <ul class="sub-menu">
+                    <li><a class="link_name" href="#">Pelanggan</a></li>
+                    <li><a href="<?php echo "../pelanggan/add_pelanggan.php"; ?>">Tambah Data</a></li>
+                    <li><a href="<?php echo "../pelanggan/list_pelanggan.php"; ?>">List Pelanggan </a></li>
+                </ul>
+            </li>
+            <li>
+                <div class="iocn-link">
+                    <a href=" # ">
+                        <i class='bx bxs-t-shirt'></i>
+                        <span class="link_name">Paket Laundry</span>
+                    </a>
+                    <i class='bx bxs-chevron-down arrow'></i>
+                </div>
+                <ul class="sub-menu">
+                    <li><a class="link_name" href="#">Paket Laundry</a></li>
+                    <li><a href="<?php echo "../paketlaundry/add_paketlaundry.php "; ?>">Tambah Paket Laundry </a></li>
+                    <li><a href="<?php echo "../paketlaundry/paketlaundry.php "; ?>">List Paket Laundry</a></li>
+                </ul>
+            </li>
+            <li>
+                <div class="iocn-link">
+                    <a href=" # ">
+                        <i class='bx bxs-user-voice'></i>
+                        <span class="link_name">Karyawan</span>
+                    </a>
+                    <i class='bx bxs-chevron-down arrow'></i>
+                </div>
+                <ul class="sub-menu">
+                    <li><a href="<?php echo "../karyawan/add_karyawan.php"; ?>">Tambah Karyawan</a></li>
+                    <li><a href="<?php echo "../karyawan/karyawan.php"; ?>">List Karyawan</a></li>
+                </ul>
+            </li>
+            <li>
+                <div class="iocn-link">
+                    <a href=" # ">
+                        <i class='bx bx-transfer'></i>
+                        <span class="link_name">Transaksi</span>
+                    </a>
+                    <i class='bx bxs-chevron-down arrow'></i>
+                </div>
+                <ul class="sub-menu">
+                    <li><a class="link_name" href="<?php echo "transupn.php"; ?>">Transaksi</a></li>
+                    <li><a href="<?php echo "../transaksi/add_transaksi.php"; ?>">Tambah Transaksi</a></li>
+                    <li><a href="<?php echo "../transaksi/transaksi.php"; ?>">List Transaksi</a></li>
+                </ul>
+            </li>
+            
+        </ul>
+    </div>
+
+    <section class="dashboard">
+        <div class="top">
+            <div class="sidebar-button">
+                <i class='bx bx-menu sidebar-toggle'></i>
+
+            </div>
+            <div class="search-box">
+                <input type="text" placeholder="Search...">
+                <i class='bx bx-search'></i>
+            </div>
+            <div class="profile-details">
+                <span class="admin_name">Admin</span>
+                <i class='bx bx-chevron-down'></i>
+            </div>
+        </div>
+
+        <div class="dash-content">
+            <div class="overview">
+                <div class="title">
+                    <i class="uil uil-tachometer-fast-alt"></i>
+                    <span class="text">Home</span>
+                </div>
+
+                <div class="boxes">
+                    <div class="box box1">
+                        <i class="uil uil-user"></i>
+                        <span class="text">Jumlah Pelanggan</span>
+                        <span class="number">27</span>
+                    </div>
+                    <div class="box box2">
+                        <i class="uil uil-comments"></i>
+                        <span class="text">Jumlah Paket Cuci</span>
+                        <span class="number">8</span>
+                    </div>
+                    <div class="box box3">
+                        <i class="uil uil-share"></i>
+                        <span class="text">Jumlah Karyawan</span>
+                        <span class="number">12</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="activity">
+                <div class="title">
+                    <i class="uil uil-clock-three"></i>
+                    <span class="text">Recent Transactions</span>
+                </div>
+
+                <div class="activity-data">
+                    <!--isi-->
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <!--<script src="script.js"></script>-->
+    <script>
+        const body = document.querySelector("body"),
+            modeToggle = body.querySelector(".mode-toggle");
+        sidebar = body.querySelector(".sidebar");
+        sidebarToggle = body.querySelector(".sidebar-toggle");
+
+        let arrow = document.querySelectorAll(".arrow");
+        for (var i = 0; i < arrow.length; i++) {
+            arrow[i].addEventListener("click", (e) => {
+                let arrowParent = e.target.parentElement.parentElement; //selecting main parent of arrow
+                arrowParent.classList.toggle("showMenu");
+            });
+        }
+
+        let getStatus = localStorage.getItem("status");
+        if (getStatus && getStatus === "close") {
+            sidebar.classList.toggle("close");
+        }
+
+        sidebarToggle.addEventListener("click", () => {
+            sidebar.classList.toggle("close");
+            if (sidebar.classList.contains("close")) {
+                localStorage.setItem("status", "close");
+            } else {
+                localStorage.setItem("status", "open");
+            }
+        })
+
+
+        sidebarToggle.onclick = function() {
+            sidebar.classList.toggle("active");
+            if (sidebar.classList.contains("active")) {
+                sidebarToggle.classList.replace("bx-menu", "bx-menu-alt-right");
+            } else
+                sidebarToggle.classList.replace("bx-menu-alt-right", "bx-menu");
+        }
+    </script>
+</body>
+
+</html>
