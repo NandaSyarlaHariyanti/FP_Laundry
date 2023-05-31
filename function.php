549f1a21d67977c122170c3ab6c6c057672382a3
Dhepp2002 <125804344+Dhepp2002@users.noreply.github.com>
2023-05-31 21:44:04 +0700

    Update function.php

--- function.php
+++ function.php
@@ -18,3 +18,29 @@ function addpaket($data)
     return $result;
 }
 
+function updatepaket($data)
+{
+    global $conn;
+
+    $id_paket = $data["id_paket"];
+    $paket = htmlspecialchars($data["paket"]);
+    $harga_kilo = htmlspecialchars($data["harga_kilo"]);
+    $deskripsi = htmlspecialchars($data["deskripsi"]);
+
+    $query = "UPDATE paket_cuci SET 
+                paket = :paket,
+                harga_kilo = :harga_kilo,
+                deskripsi = :deskripsi
+                WHERE id_paket = :id_paket";
+
+    $statement = $conn->prepare($query);
+    $statement->bindParam(':paket', $paket);
+    $statement->bindParam(':harga_kilo', $harga_kilo);
+    $statement->bindParam(':deskripsi', $deskripsi);
+    $statement->bindParam(':id_paket', $id_paket);
+
+    $result = $statement->execute();
+
+    return $result;
+}
+
