e45fe3a726cfbb43b06255cf43887bdd3b65712f
AnnisaRachmania <126047922+AnnisaRachmania@users.noreply.github.com>
2023-06-07 19:37:09 +0700

    Update transaksi.php

--- transaksi/transaksi.php
+++ transaksi/transaksi.php
@@ -1,3 +1,5 @@
+<?php include('../conn.php'); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,23 +9,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="styletransaksi.css">
+    <link rel="stylesheet" href="styletransaksi2.css">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
     <title>BINATO (FP)</title>
 </head>
 
 <body>
     <div class="sidebar close">
-
         <div class="logo-details">
             <i class='bx bxs-washer'></i>
             <span class="logo_name">BINATO</span>
-
         </div>
 
         <ul class="nav-links">
             <li>
-                <a href="<?php echo "dashboard.php"; ?>" class="active">
+                <a href="<?php echo "../admin/dashboard.php"; ?>" class="active">
                     <i class='bx bxs-home-smile'></i>
                     <span class="link_name">Home</span>
                 </a>
@@ -41,7 +44,7 @@
                 </div>
                 <ul class="sub-menu">
                     <li><a class="link_name" href="#">Pelanggan</a></li>
-                    <li><a href="<?php echo "../pelanggan/list_pelanggan.php"; ?>">List Pelanggan </a></li>
+                    <li><a href="<?php echo "../pelanggan/list_pelanggan.php"; ?>">List Pelanggan</a></li>
                     <li><a href="<?php echo "../pelanggan/add_pelanggan.php"; ?>">Tambah Pelanggan</a></li>
                 </ul>
             </li>
@@ -85,7 +88,6 @@
                     <li><a href="<?php echo "../transaksi/add_transaksi.php"; ?>">Tambah Transaksi</a></li>
                 </ul>
             </li>
-
         </ul>
     </div>
 
@@ -93,7 +95,6 @@
         <div class="top">
             <div class="sidebar-button">
                 <i class='bx bx-menu sidebar-toggle'></i>
-
             </div>
             <div class="search-box">
                 <input type="text" placeholder="Search...">
@@ -109,40 +110,88 @@
             <div class="overview">
                 <div class="title">
                     <i class="uil uil-tachometer-fast-alt"></i>
-                    <span class="text">Home</span>
+                    <span class="text">List Transaksi</span>
                 </div>
 
-                <div class="boxes">
-                    <div class="box box1">
-                        <i class="uil uil-user"></i>
-                        <span class="text">Jumlah Pelanggan</span>
-                        <span class="number">27</span>
+                <div class="card-body">
+                    <div class="d-flex justify-content-start mb-4">
+                        <a href="../transaksi/add_transaksi.php" class="btn btn-sm btn-primary mr-3">
+                            <i class="fas fa-plus fa-sm text-white"></i> Tambah Transaksi
+                        </a>
+                        <a href="../transaksi/cetaktransaksi.php" target="_blank" class="btn btn-sm btn-info mr-3">
+                            <i class="fas fa-download fa-sm text-white"></i> Cetak Transaksi</a>
                     </div>
-                    <div class="box box2">
-                        <i class="uil uil-comments"></i>
-                        <span class="text">Jumlah Paket Cuci</span>
-                        <span class="number">8</span>
-                    </div>
-                    <div class="box box3">
-                        <i class="uil uil-share"></i>
-                        <span class="text">Jumlah Karyawan</span>
-                        <span class="number">12</span>
-                    </div>
-                </div>
-            </div>
+                    <br>
+                    <table id="dataTables" class="table table-hover">
+                        <thead>
+                            <tr style="text-align: center;">
+                                <th>No</th>
+                                <th>Id Transaksi</th>
+                                <th>Tanggal Transaksi</th>
+                                <th>Nama Pelanggan</th>
+                                <th>Nama Paket</th>
+                                <th>Quantity</th>
+                                <th>Biaya</th>
+                                <th>Bayar</th>
+                                <th>Kembalian</th>
+                                <th>Keterangan</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
 
-            <div class="activity">
-                <div class="title">
-                    <i class="uil uil-clock-three"></i>
-                    <span class="text">Recent Transactions</span>
-                </div>
+                        <tbody>
+                            <?php
+                            $query = "SELECT transaksi.id_transaksi, transaksi.tanggal , pelanggan.nama_pelanggan, paket_cuci.paket, transaksi.qty, transaksi.biaya, transaksi.bayar, transaksi.kembalian 
+                                        FROM transaksi 
+                                        INNER JOIN pelanggan 
+                                        ON transaksi.id_pelanggan = pelanggan.id_pelanggan
+                                        INNER JOIN paket_cuci 
+                                        ON transaksi.id_paket = paket_cuci.id_paket 
+                                        ORDER BY transaksi.id_transaksi ASC";
+                            $result = $conn->query($query);
+                            ?>
 
-                <div class="activity-data">
-                    <!--isi-->
+                            <?php $no = 1; while ($data = $result->fetch(PDO::FETCH_ASSOC)) : ?>
+                                <tr>
+                                    <td style="text-align: center;"><?= $no++; ?></td>
+                                    <td style="text-align: center;"><?= $data['id_transaksi']; ?></td>
+                                    <td style="text-align: center;"><?= $data['tanggal']; ?></td>
+                                    <td style="text-align: center;"><?= $data['nama_pelanggan']; ?></td>
+                                    <td style="text-align: center;"><?= $data['paket']; ?></td>
+                                    <td style="text-align: center;"><?= $data['qty']; ?></td>
+                                    <td style="text-align: center;"><?= $data['biaya']; ?></td>
+                                    <td style="text-align: center;"><?= $data['bayar']; ?></td>
+                                    <td style="text-align: center;"><?= $data['kembalian']; ?></td>
+                                    <td style="text-align: center;">
+                                        <?php 
+                                            if ( $data['kembalian'] < 0 ) {
+                                                echo "<span class='keterangan-lunas'>Belum Lunas</span>";
+                                            } else {
+                                                echo "<span class='keterangan-belum-lunas'>Lunas</span>";
+                                            }
+                                        ?>
+                                    </td>
+                                    <td style="text-align: center;">
+                                    <div class="btn-group">
+                                            <a href="<?php echo "../transaksi/updatetransaksi.php?id_transaksi=" . $data['id_transaksi']; ?>" class="btn btn-outline-warning btn-sm">
+                                                <i class="fas fa-edit"></i>
+                                            </a>
+                                            <form method="POST" action="../transaksi/hapustransaksi.php" style="display: inline;">
+                                                <input class="bx bxs-delete" type="hidden" name="id_transaksi" value="<?php echo $data['id_transaksi']; ?>">
+                                                <button class="btn btn-outline-danger btn-sm" type="submit" name="deletes">
+                                                    <i class="fas fa-trash"></i>
+                                                </button>
+                                            </form>
+                                    </div>
+                                    </td>
+
+                                </tr>
+                            <?php endwhile; ?>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
-
     </section>
 
     <!--<script src="script.js"></script>-->
@@ -155,7 +204,7 @@
         let arrow = document.querySelectorAll(".arrow");
         for (var i = 0; i < arrow.length; i++) {
             arrow[i].addEventListener("click", (e) => {
-                let arrowParent = e.target.parentElement.parentElement; //selecting main parent of arrow
+                let arrowParent = e.target.parentElement.parentElement;
                 arrowParent.classList.toggle("showMenu");
             });
         }
