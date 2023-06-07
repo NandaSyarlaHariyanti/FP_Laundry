4c4dbc1dca2f6dedfa4c57a301a7c415d2eace71
Aura Choirun Nisa <120537279+Auraachn@users.noreply.github.com>
2023-06-07 15:32:49 +0700

    Update css on cetakkaryawan.php

--- karyawan/cetakkaryawan.php
+++ karyawan/cetakkaryawan.php
@@ -7,22 +7,37 @@ include('../function.php');
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="stylekaryawan.css">
-    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
-    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
     <title>BINATO (FP)</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            margin: 30px 40px;
+            width: 90%;
+        }
+
+        th,
+        td {
+            padding: 8px;
+            border: 1px solid #ddd;
+            max-width: auto;
+            text-align: center;
+        }
+
+        td.description {
+            max-width: 250px;
+            word-wrap: break-word;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
 </head>
 
 <body onload="window.print()">
-    <div class="card-body">
-        <table id="dataTables" class="table table-hover">
+    <div>
+        <table id="dataTables">
             <thead>
                 <tr style="text-align: center;">
                     <th>No</th>
