52ea0e3ccabe12428473e9ab45a8484168d676e5
Dhepp2002 <125804344+Dhepp2002@users.noreply.github.com>
2023-06-07 13:32:53 +0700

    Update list_paketlaundry.php

--- paketlaundry/list_paketlaundry.php
+++ paketlaundry/list_paketlaundry.php
@@ -14,10 +14,99 @@ include('../function.php');
     <link rel="stylesheet" href="stylepaketlaundry.css">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
     <title>BINATO (FP)</title>
+    <style>
+        .card-body {
+            margin: 10px 10px;
+        }
+
+        .button-group {
+            display: flex;
+            justify-content: flex-start;
+            margin-bottom: 4px;
+        }
+
+        .button-tambah {
+            background-color: #007bff;
+            border-radius: 5px;
+            color: #fff;
+            border: none;
+            padding: 0.5rem;
+            margin-right: 3px;
+            text-decoration: none;
+        }
+
+        .button-cetak {
+            background-color: #17a2b8;
+            border-radius: 5px;
+            color: #fff;
+            border: none;
+            padding: 0.5rem;
+            margin-right: 3px;
+            text-decoration: none;
+        }
+
+
+        table {
+            border-collapse: collapse;
+            margin: 30px 40px;
+            width: 90%;
+        }
+
+        th,
+        td {
+            padding: 8px;
+            border: 1px solid #ddd;
+            max-width: auto;
+            text-align: center;
+        }
+
+        td.description {
+            max-width: 250px;
+            word-wrap: break-word;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        .btn {
+            display: inline-block;
+            padding: 6px 12px;
+            font-size: 14px;
+            text-align: center;
+            text-decoration: none;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            color: #333;
+            margin-right: 5px;
+        }
+
+        .btn-edit {
+            background-color: transparent;
+            border-color: #ffc107;
+            color: #ffc107;
+        }
+
+        .btn-hapus {
+            background-color: transparent;
+            border-color: #dc3545;
+            color: #dc3545;
+        }
+
+        .btn-edit:active,
+        .btn-edit:hover {
+            background-color: #ffc107;
+            color: #000;
+        }
+
+        .btn-hapus:active,
+        .btn-hapus:hover {
+            background-color: #dc3545;
+            color: #fff;
+        }
+    </style>
 </head>
 
 <body>
@@ -31,13 +120,10 @@ include('../function.php');
 
         <ul class="nav-links">
             <li>
-                <a href="<?php echo "dashboard.php"; ?>" class="active">
+                <a href="<?php echo "../admin/dashboard.php"; ?>" class="active">
                     <i class='bx bxs-home-smile'></i>
                     <span class="link_name">Home</span>
                 </a>
-                <ul class="sub-menu blank">
-                    <li><a class="link_name" href="<?php echo "../admin/dashboard.php"; ?>">Home</a></li>
-                </ul>
             </li>
             <li>
                 <div class="iocn-link">
@@ -122,100 +208,65 @@ include('../function.php');
             </div>
             <div class="card-body">
                 <!-- START: Button -->
-                <div class="d-flex justify-content-start mb-4">
-                    <a href="../paketlaundry/add_paketlaundry.php" type="button" class="btn btn-sm btn-primary mr-3">
-                        <i class="fas fa-plus fa-sm text-white">
-                        </i> Tambah Data
+                <div class="button-group">
+                    <a href="../paketlaundry/add_paketlaundry.php" class="button-tambah">
+                        <i class="fas fa-plus fa-sm"></i> Tambah Data
                     </a>
-                    &nbsp;&nbsp;
-                    <a href="../paket/cetakpaket.php" target="_blank" type="button" class="btn btn-sm btn-info mr-3">
-                        <i class="fas fa-download fa-sm text-white">
-                        </i> Cetak File</a>
+                    <span>&nbsp;&nbsp;</span>
+                    <a href="../paket/cetakpaket.php" target="_blank" class="button-cetak">
+                        <i class="fas fa-download fa-sm"></i> Cetak File
+                    </a>
                 </div>
+
                 <!-- END: Button -->
-                <table id="dataTables" class="table table-hover">
-                    <thead>
-                        <tr style="text-align: center;">
-                            <th>No</th>
-                            <th>ID Paket</th>
-                            <th>Nama Paket</th>
-                            <th>Harga Per Kilo</th>
-                            <th>Deskripsi Paket</th>
-                            <th>Action</th>
-                        </tr>
-
-                    </thead>
-                    <tbody>
-                        <?php
-                        //proses menampilkan data dari database:
-                        //siapkan query SQL
-                        $query = "SELECT * FROM paket_cuci";
-                        //eksekusi query
-                        $result = $conn->query($query);
-                        $no = 1; ?>
-
-                        <?php while ($data = $result->fetch(PDO::FETCH_ASSOC)) : ?>
-                            <tr>
-                                <td><?= $no++; ?></td>
-                                <td><?= $data['id_paket']; ?></td>
-                                <td><?= $data['paket']; ?></td>
-                                <td><?= $data['harga_kilo']; ?></td>
-                                <td><?= $data['deskripsi']; ?></td>
-                                <td>
-                                    <a href="<?php echo "updatepaket.php?id_paket=" . $data['id_paket']; ?>" class="btn btn-outline-warning btn-sm"> Update</a>
-                                    &nbsp;&nbsp;
-                                    <form method="POST" action="../paketlaundry/hapuspaket.php">
-                                        <input class="bx bxs-delete" type="hidden" name="id_paket" value="<?php echo $data['id_paket']; ?>">
-                                        <button class="btn btn-outline-danger btn-sm" type="submit" name="deletes">Delete</button>
-                                    </form>
-                                </td>
-                            </tr>
-                        <?php endwhile; ?>
-                    </tbody>
-                </table>
             </div>
+
+            <!-- END: Button -->
+            <table>
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>ID Paket</th>
+                        <th>Nama Paket</th>
+                        <th>Harga Per Kilo</th>
+                        <th>Deskripsi Paket</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    //proses menampilkan data dari database:
+                    //siapkan query SQL
+                    $query = "SELECT * FROM paket_cuci";
+                    //eksekusi query
+                    $result = $conn->query($query);
+                    $no = 1;
+                    while ($data = $result->fetch(PDO::FETCH_ASSOC)) :
+                    ?>
+                        <tr>
+                            <td><?php echo $no++; ?></td>
+                            <td><?php echo $data['id_paket']; ?></td>
+                            <td><?php echo $data['paket']; ?></td>
+                            <td><?php echo $data['harga_kilo']; ?></td>
+                            <td class="description"><?php echo $data['deskripsi']; ?></td>
+                            <td>
+                                <a href="updatepaket.php?id_paket=<?php echo $data['id_paket']; ?>" class="btn btn-edit">Edit</a>
+                                <form method="POST" action="../paketlaundry/hapuspaket.php" style="display: inline-block;">
+                                    <input type="hidden" name="id_paket" value="<?php echo $data['id_paket']; ?>">
+                                    <button type="submit" name="deletes" class="btn btn-hapus">Hapus</button>
+                                </form>
+                            </td>
+                        </tr>
+                    <?php endwhile; ?>
+                </tbody>
+            </table>
+
+        </div>
         </div>
 
     </section>
 
-    <script src="script.js"></script>
-    <script>
-        const body = document.querySelector("body"),
-            modeToggle = body.querySelector(".mode-toggle");
-        sidebar = body.querySelector(".sidebar");
-        sidebarToggle = body.querySelector(".sidebar-toggle");
-
-        let arrow = document.querySelectorAll(".arrow");
-        for (var i = 0; i < arrow.length; i++) {
-            arrow[i].addEventListener("click", (e) => {
-                let arrowParent = e.target.parentElement.parentElement; //selecting main parent of arrow
-                arrowParent.classList.toggle("showMenu");
-            });
-        }
-
-        let getStatus = localStorage.getItem("status");
-        if (getStatus && getStatus === "close") {
-            sidebar.classList.toggle("close");
-        }
-
-        sidebarToggle.addEventListener("click", () => {
-            sidebar.classList.toggle("close");
-            if (sidebar.classList.contains("close")) {
-                localStorage.setItem("status", "close");
-            } else {
-                localStorage.setItem("status", "open");
-            }
-        })
-
-
-        sidebarToggle.onclick = function() {
-            sidebar.classList.toggle("active");
-            if (sidebar.classList.contains("active")) {
-                sidebarToggle.classList.replace("bx-menu", "bx-menu-alt-right");
-            } else
-                sidebarToggle.classList.replace("bx-menu-alt-right", "bx-menu");
-        }
-    </script>
+    <script src="../script.js"></script>
 </body>
 
 </html>
