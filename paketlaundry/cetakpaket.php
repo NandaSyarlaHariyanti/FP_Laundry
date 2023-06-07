30e2a63b84200c38bb2cc85f648ddb3daf8237fb
Dhepp2002 <125804344+Dhepp2002@users.noreply.github.com>
2023-06-07 14:17:47 +0700

    Update cetakpaket.php

--- paketlaundry/cetakpaket.php
+++ paketlaundry/cetakpaket.php
@@ -7,13 +7,6 @@ include('../function.php');
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="stylepaketlaundry.css">
-    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
-    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <title>BINATO (FP)</title>
     <style>
         table {
