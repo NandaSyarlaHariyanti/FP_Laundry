acf517f430fb87448037af48653f9f107b1b5309
AnnisaRachmania <126047922+AnnisaRachmania@users.noreply.github.com>
2023-06-07 19:41:01 +0700

    Add files via upload

---
+++ transaksi/cetaktransaksi.php
@@ -0,0 +1,151 @@
+<?php 
+    include('../conn.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cetak Transaksi</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f1f1f1;
+            margin: 0;
+            padding: 0;
+        }
+
+        h2 {
+            color: #333;
+            text-align: center;
+            margin-top: 20px;
+        }
+
+        table {
+            margin: 20px auto;
+            background-color: #fff;
+            border-collapse: collapse;
+            max-width: 1000px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            border-radius: 5px;
+            font-size: 12px;
+        }
+
+        th, td {
+            padding: 10px;
+            text-align: center;
+            border-bottom: 1px solid #ddd;
+        }
+
+        th {
+            background-color: #f2f2f2;
+            color: #333;
+            font-weight: bold;
+        }
+
+        tr:last-child td {
+            border-bottom: none;
+        }
+
+        .lunas {
+            background-color: green;
+            color: white;
+            display: inline-block;
+            padding: 5px 10px;
+            border-radius: 5px;
+        }
+
+        .belum-lunas {
+            background-color: red;
+            color: white;
+            display: inline-block;
+            padding: 5px 10px;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+<body>
+    <h2>Data Transaksi Binato</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Id Transaksi</th>
+                <th>Tanggal Transaksi</th>
+                <th>Nama Pelanggan</th>
+                <th>Nama Paket</th>
+                <th>Quantity</th>
+                <th>Biaya</th>
+                <th>Bayar</th>
+                <th>Kembalian</th>
+                <th>Keterangan</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $query = "SELECT transaksi.id_transaksi, transaksi.tanggal, pelanggan.nama_pelanggan, paket_cuci.paket, transaksi.qty, transaksi.biaya, transaksi.bayar, transaksi.kembalian 
+                    FROM transaksi 
+                    INNER JOIN pelanggan ON transaksi.id_pelanggan = pelanggan.id_pelanggan
+                    INNER JOIN paket_cuci ON transaksi.id_paket = paket_cuci.id_paket 
+                    ORDER BY transaksi.id_transaksi ASC";
+
+            $result = $conn->query($query);
+            $no = 1;
+            $totalBiaya = 0; // Tambahkan variabel totalBiaya dan inisialisasi dengan 0
+            
+            while ($data = $result->fetch(PDO::FETCH_ASSOC)) :
+                $keterangan = $data['kembalian'] < 0 ? 'Belum Lunas' : 'Lunas';
+                $keteranganClass = $data['kembalian'] < 0 ? 'belum-lunas' : 'lunas';
+
+                // Tambahkan nilai biaya setiap transaksi ke totalBiaya
+                $totalBiaya += $data['biaya'];
+            ?>
+                <tr>
+                    <td><?= $no++; ?></td>
+                    <td><?= $data['id_transaksi']; ?></td>
+                    <td><?= $data['tanggal']; ?></td>
+                    <td><?= $data['nama_pelanggan']; ?></td>
+                    <td><?= $data['paket']; ?></td>
+                    <td><?= $data['qty']; ?></td>
+                    <td>Rp <?= number_format($data['biaya'], 0, ',', '.'); ?></td>
+                    <td>Rp <?= number_format($data['bayar'], 0, ',', '.'); ?></td>
+                    <td>Rp <?= number_format($data['kembalian'], 0, ',', '.'); ?></td>
+                    <td>
+                        <span class="<?= $keteranganClass; ?>">
+                            <?= $keterangan; ?>
+                        </span>
+                    </td>
+                </tr>
+            <?php endwhile; ?>
+
+            <!-- Tambahkan baris untuk menampilkan total biaya -->
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>Total Biaya</td>
+                <td>Rp <?= number_format($totalBiaya, 0, ',', '.'); ?></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+
+    <script>
+        window.print();
+
+        window.onafterprint = function() {
+            alert('Data Berhasil Dicetak');
+            window.location.href = '../transaksi/transaksi.php';
+        };
+    </script>
+
+</body>
+</html>
+
+
---
+++ transaksi/updatetransaksi.php
@@ -0,0 +1,129 @@
+<?php
+    include("../conn.php");
+
+    // Ambil ID transaksi dari URL
+    $id_transaksi = isset($_GET['id_transaksi']) ? $_GET['id_transaksi'] : '';
+
+    // Query untuk mengambil data transaksi berdasarkan id_transaksi
+    $query = "SELECT * FROM transaksi WHERE id_transaksi = :id_transaksi";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(':id_transaksi', $id_transaksi);
+    $stmt->execute();
+    $data_transaksi = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // Periksa apakah form telah disubmit
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Ambil data dari form
+        $tanggal = isset($_POST['tanggal']) ? $_POST['tanggal'] : '';
+        $id_pelanggan = isset($_POST['id_pelanggan']) ? $_POST['id_pelanggan'] : '';
+        $id_paket = isset($_POST['id_paket']) ? $_POST['id_paket'] : '';
+        $qty = isset($_POST['qty']) ? $_POST['qty'] : '';
+        $biaya = isset($_POST['biaya']) ? $_POST['biaya'] : '';
+        $bayar = isset($_POST['bayar']) ? $_POST['bayar'] : '';
+        $kembalian = isset($_POST['kembalian']) ? $_POST['kembalian'] : '';
+
+        // Lakukan proses update data transaksi ke dalam database
+        $query_update = "UPDATE transaksi SET tanggal = :tanggal, id_pelanggan = :id_pelanggan, id_paket = :id_paket, qty = :qty, biaya = :biaya, bayar = :bayar, kembalian = :kembalian WHERE id_transaksi = :id_transaksi";
+        $stmt_update = $conn->prepare($query_update);
+        $stmt_update->bindParam(':tanggal', $tanggal);
+        $stmt_update->bindParam(':id_pelanggan', $id_pelanggan);
+        $stmt_update->bindParam(':id_paket', $id_paket);
+        $stmt_update->bindParam(':qty', $qty);
+        $stmt_update->bindParam(':biaya', $biaya);
+        $stmt_update->bindParam(':bayar', $bayar);
+        $stmt_update->bindParam(':kembalian', $kembalian);
+        $stmt_update->bindParam(':id_transaksi', $id_transaksi);
+        $stmt_update->execute();
+
+        // Redirect ke halaman yang diinginkan setelah proses update selesai
+        header("Location: transaksi.php");
+        exit();
+}
+
+$stmt_1 = $conn->prepare("SELECT id_pelanggan, nama_pelanggan FROM pelanggan");
+$stmt_1->execute();
+$pelanggan = $stmt_1->fetchAll(PDO::FETCH_ASSOC);
+
+$stmt_2 = $conn->prepare("SELECT id_paket, paket FROM paket_cuci");
+$stmt_2->execute();
+$paket_cuci = $stmt_2->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-a1H+hRVW/n4h4/d73yQyRRgkFgn3jBku3a2lRlZImFHAj6rM2RUv8D6vymMBLtXPHfXE02hOlhRq/hE49lMDwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="formupdate.css">
+
+    <title>Update Transaksi</title>
+</head>
+
+<body>
+    <div class="container">
+    <div class="form-container">
+        <h2 class="form-title">Update Data Transaksi</h2>
+        <form method="post" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) . '?id_transaksi=' . $id_transaksi; ?>">
+            <!-- Form fields go here -->
+            <div>
+                <input type="hidden" name="id_transaksi" value="<?php echo $data_transaksi['id_transaksi']; ?>">
+            </div>
+            <div>
+                <label for="id_pelanggan">Nama Pelanggan</label>
+                <select name="id_pelanggan" required autofocus>
+                    <?php foreach ($pelanggan as $pelanggan_) { ?>
+                        <option value="<?php echo $pelanggan_['id_pelanggan']; ?>" <?php if ($data_transaksi['id_pelanggan'] == $pelanggan_['id_pelanggan']) echo 'selected'; ?>>
+                            <?php echo $pelanggan_['nama_pelanggan']; ?>
+                        </option>
+                    <?php } ?>
+                </select>
+            </div>
+            <div>
+                <label for="id_paket">Jenis Paket</label>
+                <select name="id_paket" required autofocus>
+                    <?php foreach ($paket_cuci as $paket_cuci_) { ?>
+                        <option value="<?php echo $paket_cuci_['id_paket']; ?>" <?php if ($data_transaksi['id_paket'] == $paket_cuci_['id_paket']) echo 'selected'; ?>>
+                            <?php echo $paket_cuci_['paket']; ?>
+                        </option>
+                    <?php } ?>
+                </select>
+            </div>
+            <div>
+                <label for="tanggal">Tanggal</label>
+                <input type="date" name="tanggal" id="tanggal" value="<?php echo $data_transaksi['tanggal']; ?>" required>
+            </div>
+            <div>
+                <label for="qty">Quantity</label>
+                <input type="text" name="qty" id="qty" value="<?php echo $data_transaksi['qty']; ?>" required>
+            </div>
+            <div>
+                <label for="biaya">Biaya</label>
+                <input type="text" name="biaya" id="biaya" value="<?php echo $data_transaksi['biaya']; ?>" required>
+            </div>
+            <div>
+                <label for="bayar">Bayar</label>
+                <input type="text" name="bayar" id="bayar" value="<?php echo $data_transaksi['bayar']; ?>" required>
+            </div>
+            <div>
+                <label for="kembalian">Kembalian</label>
+                <input type="text" name="kembalian" id="kembalian" value="<?php echo $data_transaksi['kembalian']; ?>" required>
+            </div>
+            <div class="center-button">
+                <button type="reset" class="reset"><i class="fas fa-undo"></i> Reset</button>
+                <button type="submit" name="submit" class="save"><i class="fas fa-save"></i> Save</button>
+            </div>
+            <div class="center-button">
+                <button class="back"><i class="fas fa-home"></i><a href="../transaksi/transaksi.php"> Kembali</a></button>
+            </div>
+        </form>
+    </div>
+    </div>
+</body>
+
+</html>
+
+
