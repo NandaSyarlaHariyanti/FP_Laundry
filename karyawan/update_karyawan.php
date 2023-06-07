3a033b980ff62d7a87c9d80f095a830f17dd08b7
Aura Choirun Nisa <120537279+Auraachn@users.noreply.github.com>
2023-06-07 15:56:02 +0700

    Update css on update_karyawan.php

--- karyawan/update_karyawan.php
+++ karyawan/update_karyawan.php
@@ -89,32 +89,82 @@ if (isset($_POST["submit"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style>
+        body {
+            background-image: url();
+            background-size: cover;
+            background-repeat: no-repeat;
+            height: 100vh;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+
         .container {
             display: flex;
             justify-content: center;
             align-items: center;
             height: 100%;
+            margin: 20px;
+        }
+
+        .form-container {
+            width: 500px;
+            padding: 20px;
+            padding-left: 30px;
+            background-color: #f8f9fa;
+            border-radius: 10px;
+            margin: 10px 30px;
+        }
+
+        h1 {
+            margin-bottom: 20px;
+        }
+
+        .form-control {
+            width: 90%;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            margin-top: 5px;
+            margin-bottom: 20px;
+        }
+
+        .center-button {
+            display: flex;
+            justify-content: center;
+            margin-top: 20px;
+        }
+
+        .center-button button {
+            margin-right: 10px;
+        }
+
+        .btn {
+            padding: 10px 20px;
+            background-color: #dc3545;
+            color: #fff;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            text-decoration: none;
+        }
+
+        .btn-save {
+            background-color: #28a745;
+        }
+
+        .btn-back {
+            background-color: #0056b3;
         }
     </style>
 
-    <title>Update Pelanggan</title>
+    <title>Update Karyawan</title>
 </head>
 
 <body>
     <div class="container">
-        <div class="card mt-6 mb-6">
-            <h5 class="card-header d-flex flex-row align-items-center justify-content-between">
-                <a>Edit Paket</a>
-                <a href="?page=Paket" role="button" id="dropdownMenuLink" aria-haspopup="true" aria-expanded="false">
-                    <i class="fas fa-chevron-left fa-sm fa-fw"></i>
-                </a>
-            </h5>
-
-            <div class="card-body">
-                <form method="post" action="" enctype="multipart/form-data">
+        <div class="form-container">
+            <h1>Update Data Karyawan</h1>
+            <form method="post" enctype="multipart/form-data">
                     <div>
                         <label for="id_karyawan">Id Karyawan</label>
                         <input type="text" maxlength="50" class="form-control" name="id_karyawan" value="<?= $data['id_karyawan'] ?? ''; ?>" required readonly>
@@ -133,8 +183,9 @@ if (isset($_POST["submit"])) {
 
                     <div>
                          <label for="email">Email</label>
-                         <input type="text" maxlength="50" class="form-control" name="email" value="<?= $data['email'];?>" required autofocus>
-                    </div>
+                        <div>
+                            <input type="text" maxlength="50" class="form-control" name="email" value="<?= $data['email'];?>" required autofocus>
+                        </div>
 
                     <div>
                          <label for="no_hp">No Handphone</label>
@@ -157,10 +208,14 @@ if (isset($_POST["submit"])) {
                          </div>
                     </div>
 
-                    <div class="card-footer text-center">
-                        <button type="reset" class="btn btn-danger mr-2"><i class="fas fa-undo"></i> Reset</button>
-                        <button type="submit" name="submit" class="btn btn-success"><i class="fas fa-save"></i> Save Change</button>
+                    <div class="center-button">
+                        <button type="reset" class="btn btn-reset"><i class="fas fa-undo"></i> Reset</button>
+                        <button type="submit" name="submit" class="btn btn-save"><i class="fas fa-save"></i>Simpan Perubahan</button>
                     </div>
+                    <div class="center-button">
+                    <a href="../kartawan/list_karyawan.php" class="btn btn-back">Kembali</a>
+                    </div>
+
                 </form>
             </div>
         </div>
