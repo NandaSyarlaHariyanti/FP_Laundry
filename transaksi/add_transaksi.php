0d910f6e71bb9c73c3549f53e34fa84c7b217f3b
AnnisaRachmania <126047922+AnnisaRachmania@users.noreply.github.com>
2023-06-07 19:39:23 +0700

    Update add_transaksi.php

--- transaksi/add_transaksi.php
+++ transaksi/add_transaksi.php
@@ -1,86 +1,131 @@
-<?php
-	if( isset($_POST['submit'])) {
-        $id           		= addslashes($_POST['id_pelanggan']);
-        $nama       		= addslashes($_POST['nama_pelanggan']);
-		$alamat       		= addslashes($_POST['alamat_pelanggan']);
-        $nohp       		= stripslashes($_POST['no_hp_pelanggan']);
+<<?php
+    include('../conn.php');
 
-    $image = uploadImage('upload/pelanggan');
-    if ( !$image ) {
-      return false;
-    }
-    $query = "INSERT INTO pelanggan VALUES ( '$id', '$nama', '$alamat', '$nohp')";
-    if( queryData($query) > 0){
-        echo "
-            <script>
-                alert('Data berhasil ditambahkan');
-                document.location.href = '?page=Pelanggan';
-            </script>";
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $id_transaksi = $_POST['id_transaksi'];
+        $tanggal = $_POST['tanggal'];
+        $qty = $_POST['qty'];
+        $biaya = $_POST['biaya'];
+        $bayar = $_POST['bayar'];
+        $kembalian = $_POST['kembalian'];
+        $id_pelanggan = $_POST['id_pelanggan'];
+        $id_paket = $_POST['id_paket'];
+
+        // Menyiapkan pernyataan (prepared statement) untuk kueri INSERT
+        $query = $conn->prepare("INSERT INTO transaksi (id_transaksi, id_pelanggan, id_paket, tanggal, qty, biaya, bayar, kembalian) 
+                                VALUES (:id_transaksi, :id_pelanggan, :id_paket, :tanggal, :qty, :biaya, :bayar, :kembalian)");
+
+        // Mengikat nilai-nilai parameter ke pernyataan
+        $query->bindParam(':id_transaksi', $id_transaksi);
+        $query->bindParam(':id_pelanggan', $id_pelanggan);
+        $query->bindParam(':id_paket', $id_paket);
+        $query->bindParam(':tanggal', $tanggal);
+        $query->bindParam(':qty', $qty);
+        $query->bindParam(':biaya', $biaya);
+        $query->bindParam(':bayar', $bayar);
+        $query->bindParam(':kembalian', $kembalian);
+
+        // Mengeksekusi pernyataan (prepared statement)
+        if ($query->execute()) {
+            echo "
+                <script>
+                    alert('Data berhasil ditambahkan');
+                    window.location.href = '../transaksi/transaksi.php';
+                </script>";
         } else {
             echo "
                 <script>
                     alert('Data gagal ditambahkan');
-                    document.location.href = '?page=Pelanggan';
+                    window.location.href = '../transaksi/transaksi.php';
                 </script>";
+        }        
     }
-}
-?>   
+
+    $stmt = $conn->prepare("SELECT id_pelanggan, nama_pelanggan FROM pelanggan");
+    $stmt->execute();
+    $pelanggan = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $stmt_2 = $conn->prepare("SELECT id_paket, paket FROM paket_cuci");
+    $stmt_2->execute();
+    $paket_cuci = $stmt_2->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <style>
-        body {
-            height: 100vh;
-        }
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-a1H+hRVW/n4h4/d73yQyRRgkFgn3jBku3a2lRlZImFHAj6rM2RUv8D6vymMBLtXPHfXE02hOlhRq/hE49lMDwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="formtransaksi.css">
 
-        .container {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100%;
-        }
-
-        .form-container {
-            width: 600px;
-            padding: 20px;
-            background-color: #f8f9fa;
-            border-radius: 10px;
-        }
-
-        .center-button {
-            display: flex;
-            justify-content: center;
-            margin-top: 20px;
-        }
-
-        .center-button button {
-            margin-right: 10px;
-        }
-    </style>
-    
-    <title>Tambah Pelanggan</title>
+    <title>Tambah Transaksi</title>
 </head>
+
 <body>
     <div class="container">
         <div class="form-container">
-            <h2>Tambah Data Transaksi</h2>
-            <form method="post" enctype="multipart/form-data">
-                
+            <h2 class="form-title">Tambah Data Transaksi</h2>
+            <form method="post" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                <!-- Form fields go here -->
+                <div class="form-group">
+                    <label for="id_transaksi">ID Transaksi</label>
+                    <input type="text" maxlength="50" name="id_transaksi" id="id_transaksi" placeholder="Masukkan ID Transaksi" required autofocus>
+                </div>
+                <div class="form-group">
+                    <label for="id_pelanggan">Nama Pelanggan</label>
+                    <select name="id_pelanggan" placeholder="Pilih Nama Pelanggan" required autofocus>
+                        <?php foreach ($pelanggan as $pelanggan_) { ?>
+                            <option value="<?php echo $pelanggan_['id_pelanggan']; ?>">
+                                <?php echo $pelanggan_['nama_pelanggan']; ?>
+                            </option>
+                        <?php } ?>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="id_paket">Jenis Paket</label>
+                    <select name="id_paket" placeholder="Pilih Jenis Paket" required autofocus>
+                        <?php foreach ($paket_cuci as $paket_cuci_) { ?>
+                            <option value="<?php echo $paket_cuci_['id_paket']; ?>">
+                                <?php echo $paket_cuci_['paket']; ?>
+                            </option>
+                        <?php } ?>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="tanggal">Tanggal</label>
+                    <input type="date" maxlength="30" name="tanggal" id="tanggal" placeholder="Masukkan tanggal transaksi" required>
+                </div>
+                <div class="form-group">
+                    <label for="qty">Quantity</label>
+                    <input type="text" maxlength="100" name="qty" id="qty" placeholder="Masukkan jumlah quantity (kg)" required>
+                </div>
+                <div class="form-group">
+                    <label for="biaya">Biaya</label>
+                    <input type="text" maxlength="100" name="biaya" id="biaya" placeholder="Masukkan total harga" required>
+                </div>
+                <div class="form-group">
+                    <label for="bayar">Bayar</label>
+                    <input type="text" maxlength="100" name="bayar" id="bayar" placeholder="Masukkan total biaya yang dibayarkan" required>
+                </div>
+                <div class="form-group">
+                    <label for="kembalian">Kembalian</label>
+                    <input type="text" maxlength="100" name="kembalian" id="kembalian" placeholder="Tulis kembalian" required>
+                </div>
                 <div class="center-button">
-                    <button type="reset" class="btn btn-danger mr-2"><i class="fas fa-undo"></i> Reset</button>
-                    <button type="submit" name="submit" class="btn btn-success"><i class="fas fa-save"></i> Save</button>
+                    <button type="reset" class="reset"><i class="fas fa-undo"></i> Reset</button>
+                    <button type="submit" name="submit" class="save"><i class="fas fa-save"></i> Save</button>
+                    
+                </div>
+                <div class="center-button">
+                    <button class="back"><i class="fas fa-home"></i><a href="../transaksi/transaksi.php"> Kembali</a></button>
                 </div>
             </form>
         </div>
     </div>
-
-    <!-- Popper and Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
+
 </html>
+
