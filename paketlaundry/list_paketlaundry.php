5320dc13b11284fd00ef3251f77b0fb669f11e08
Aura Choirun Nisa <120537279+Auraachn@users.noreply.github.com>
2023-06-08 19:45:57 +0700

    Update link list_paketlaundry.php

--- paketlaundry/list_paketlaundry.php
+++ paketlaundry/list_paketlaundry.php
@@ -163,7 +163,7 @@ include('../function.php');
                     <i class='bx bxs-chevron-down arrow'></i>
                 </div>
                 <ul class="sub-menu">
-                    <li><a href="<?php echo "../karyawan/karyawan.php"; ?>">List Karyawan</a></li>
+                    <li><a href="<?php echo "../karyawan/list_karyawan.php"; ?>">List Karyawan</a></li>
                     <li><a href="<?php echo "../karyawan/add_karyawan.php"; ?>">Tambah Karyawan</a></li>
                 </ul>
             </li>
