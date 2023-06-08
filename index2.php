e3921fd92fc0d5b1118914d0cc7d40ecad1e7750
Dhepp2002 <125804344+Dhepp2002@users.noreply.github.com>
2023-06-08 14:31:35 +0700

    Create index2.php

---
+++ index2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Login</title>
+    <link href='https://fonts.googleapis.com/css?family=Fredoka One' rel='stylesheet'>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="home">
+        <p class="hallo">WELCOME TO</p>
+        <p class="hallo2">BINATO'S LAUNDRY</p>
+
+        <div class="tombol">
+            <div class="admin">
+                <a href="admin/loginadmin.php" class="my-button first-button">Admin</a>
+            </div>
+            <div class="pelanggan">
+                <a href="pelanggan/loginpelanggan.php" class="my-button second-button">Pelanggan</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
