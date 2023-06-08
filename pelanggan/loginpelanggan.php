605d23978157693557fc435d368a33b0b8c74ded
Dhepp2002 <125804344+Dhepp2002@users.noreply.github.com>
2023-06-08 13:38:08 +0700

    Update loginpelanggan.php

--- pelanggan/loginpelanggan.php
+++ pelanggan/loginpelanggan.php
@@ -51,7 +51,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Login Pelanggan</title>
-    <link rel="stylesheet" href="../pelanggan/styleloginn.css">
+    <link rel="stylesheet" href="../admin/login.css">
     <style>
     </style>
 
