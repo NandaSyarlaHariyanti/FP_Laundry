95d52a2432bba067e680907b8f3e4044f8743ea0
Dhepp2002 <125804344+Dhepp2002@users.noreply.github.com>
2023-06-08 13:31:49 +0700

    Update loginadmin.php

--- admin/loginadmin.php
+++ admin/loginadmin.php
@@ -1,22 +1,61 @@
+<?php
+require_once('../conn.php');
+
+$errorMessage = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if ($username === 'admin' && $password === '12345') {
+        // Set cookie dengan nama "login" dan nilai "true" dengan masa berlaku 1 jam (3600 detik)
+        setcookie("login", "true", time() + 3600, '/');
+        // Redirect ke halaman selamat datang atau halaman berikutnya
+        echo "
+            <script>
+                alert('Login Berhasil');
+                document.location.href = '../admin/dashboard.php';
+            </script> 
+        ";
+        exit;
+    } else {
+        // Login gagal
+        echo "
+            <script>
+                alert('Username atau password salah!');
+                document.location.href = '../admin/loginadmin.php';
+            </script> 
+        ";
+        exit;
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../admin/login.css">
     <title>Form Login Admin</title>
-    <link rel="stylesheet" href="stylelogin.css">
-    
+    <style>
+    </style>
 </head>
+
 <body>
-    <form method="post"  action="dashboard.php" >
+    <?php if ($errorMessage !== '') : ?>
+        <p><?php echo $errorMessage; ?></p>
+    <?php endif; ?>
+
+    <form method="POST" action="loginadmin.php">
         <h2>Login</h2>
-
-        <input type="text" id="username" name="username" placeholder="username" required><br> <br>
-        
-        <input type="text" id="password" name="password" placeholder="password" required><br> <br>
-
+        <input type="text" id="username" name="username" placeholder="Username" required><br><br>
+        <input type="password" id="password" name="password" placeholder="Password" required><br><br>
         <input type="submit" name="submit" value="Login">
+        <a href="../index.php" class="btn-back">Kembali</a>
     </form>
 </body>
+
 </html>
