6ca92bcee9137024ac689e6d8ab1736a1b1ec0b5
NandaSyarlaHariyanti <125377979+NandaSyarlaHariyanti@users.noreply.github.com>
2023-06-09 11:55:55 +0700

    Update riwayattransaksi.php

--- pelanggan/riwayattransaksi.php
+++ pelanggan/riwayattransaksi.php
@@ -30,7 +30,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Daftar Transaksi</title>
-    <link rel="stylesheet" href="styletransaksi.css">
+    <link rel="stylesheet" href="styleriwayattransaksi.css">
 </head>
 <body>
     
