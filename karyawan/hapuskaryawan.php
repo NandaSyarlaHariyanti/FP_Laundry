3fac4133091058b43778fad8dc9b48bd7dddd47f
Dhepp2002 <125804344+Dhepp2002@users.noreply.github.com>
2023-06-10 22:28:33 +0700

    Update hapuskaryawan.php

--- karyawan/hapuskaryawan.php
+++ karyawan/hapuskaryawan.php
@@ -4,16 +4,19 @@ include('../conn.php');
 if (isset($_POST['deletes'])) {
     $id_karyawan_del = $_POST['id_karyawan'];
 
-    $get_image = "SELECT 'image' FROM karyawan WHERE id_karyawan='$id_karyawan_del'";
+    $get_image = "SELECT image FROM karyawan WHERE id_karyawan = :id_karyawan";
     $stmt = $conn->prepare($get_image);
     $stmt->bindParam(':id_karyawan', $id_karyawan_del);
-        $stmt->execute();
-        $image_lama = $stmt->fetch(PDO::FETCH_ASSOC);
+    $stmt->execute();
+    $image_lama = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        unlink("image/" . $image_lama['image']);
+    unlink("image/" . $image_lama['image']);
 
-    $query = "DELETE FROM karyawan WHERE id_karyawan = '$id_karyawan_del'"; 
-    $result = $conn->query($query);
+    $query = "DELETE FROM karyawan WHERE id_karyawan = :id_karyawan_del"; 
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(':id_karyawan_del', $id_karyawan_del);
+    $result = $stmt->execute();
+
     if ($result) {
         echo "
           <script>
